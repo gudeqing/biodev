f00b2396f0b189ee713353985c2fc6034e96be39
gudeqing <822466659@qq.com>
2019-02-22 17:44:15 +0800

    None

--- mycolors.py
+++ mycolors.py
@@ -27,3 +27,79 @@ color_pool = cl.to_rgb(p[::-1])
 
 # 获取12个漂亮的颜色
 color_pool = cl.scales['12']['qual']['Paired']
+
+
+
+
+
+def ucsc2gencode(infile):
+    return dict(x.strip().split()[:2] for x in open(infile))
+
+
+def replace_ucsc_with_gencode(infile, outfile, convert_file='GRCh38_UCSC2gencode.txt'):
+    convert_dict = ucsc2gencode(convert_file)
+    with open(infile) as fr, open(outfile, 'w') as fw:
+        for line in fr:
+            chr_name = line.split()[0]
+            if chr_name in convert_dict:
+                line = line.replace(chr_name, convert_dict[chr_name], 1)
+                fw.write(line)
+            else:
+                fw.write(line)
+
+def filter_bed_of_target_chr(infile, outfile, chr_list_file='primary_assembly.chr.list'):
+    chr_list = [x.strip() for x in  open(chr_list_file)]
+    with open(infile) as fr, open(outfile, 'w') as fw:
+        for line in fr:
+            chr_name = line.split()[0]
+            if chr_name in chr_list:
+                fw.write(line)
+            else:
+                pass
+
+
+if __name__ == '__main__':
+    def introduce_command(func):
+        import argparse
+        import inspect
+        import json
+        import time
+        if isinstance(func, type):
+            description = func.__init__.__doc__
+        else:
+            description = func.__doc__
+        parser = argparse.ArgumentParser(description=description, formatter_class=argparse.RawTextHelpFormatter)
+        func_args = inspect.getfullargspec(func)
+        arg_names = func_args.args
+        arg_defaults = func_args.defaults
+        arg_defaults = ['None']*(len(arg_names) - len(arg_defaults)) + list(arg_defaults)
+        for arg, value in zip(arg_names, arg_defaults):
+            if arg == 'self':
+                continue
+            if value == 'None':
+                parser.add_argument('-'+arg, required=True, metavar=arg)
+            elif type(value) == bool:
+                if value:
+                    parser.add_argument('--'+arg, action="store_false", help='default: True')
+                else:
+                    parser.add_argument('--'+arg, action="store_true", help='default: False')
+            elif value is None:
+                parser.add_argument('-' + arg, default=value, metavar='Default:' + str(value), )
+            else:
+                parser.add_argument('-' + arg, default=value, type=type(value), metavar='Default:' + str(value), )
+        if func_args.varargs is not None:
+            print("warning: *varargs is not supported, and will be neglected! ")
+        if func_args.varkw is not None:
+            print("warning: **keywords args is not supported, and will be neglected! ")
+        args = parser.parse_args().__dict__
+        with open("Argument_detail.json", 'w') as f:
+            json.dump(args, f, indent=2, sort_keys=True)
+        start = time.time()
+        func(**args)
+        print("total time: {}s".format(time.time() - start))
+
+    introduce_command(filter_bed_of_target_chr)
+
+
+
+
