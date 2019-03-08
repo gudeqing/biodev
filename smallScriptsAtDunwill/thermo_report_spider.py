6556bbdf4500a3a464c0e8e4d39238b2226ac747
gudeqing <822466659@qq.com>
2019-03-08 16:13:29 +0800

    修改生成目标文件

--- smallScriptsAtDunwill/thermo_report_spider.py
+++ smallScriptsAtDunwill/thermo_report_spider.py
@@ -1,8 +1,10 @@
+# coding=utf-8
 import requests
 from bs4 import BeautifulSoup as bs
 import json
 import re
 import os
+from collections import OrderedDict
 from pprint import pprint
 
 
@@ -288,11 +290,16 @@ def main(url='http://10.62.2.15/report/146/', login_url='http://10.62.2.15/login
                         sample_detail.setdefault(each['barcode_name'], dict())
                         sample_detail[each['barcode_name']].update(each)
 
-    # write out overall info
+    # 过滤overall info
     import pandas as pd
-    pd.DataFrame(overall_info, index=['all']).to_csv('overall.info.xls', index=False, header=True, sep='\t')
+    final_overall = dict()
+    for k, v in overall_info.items():
+        if k.endswith('Args'):
+            continue
+        final_overall[k] = v
 
-    tmp_dict = dict()
+    # 过滤sample detail
+    sample_detail_dict = dict()
     for x, y in sample_detail.items():
         tmp_y = dict()
         for k, v in y.items():
@@ -309,9 +316,188 @@ def main(url='http://10.62.2.15/report/146/', login_url='http://10.62.2.15/login
             elif k == 'fm3_pass80':
                 k = 'LOD %'
             tmp_y[k] = v
-        tmp_dict[x] = tmp_y
+        sample_detail_dict[x] = tmp_y
 
-    pd.DataFrame(tmp_dict).to_csv('sample_detail.xls', header=True, index=True, sep='\t')
+    overall_target_fields = "Run Name, Run Date, Report Name, Report Date, Research Application, Projects, Chip Type, " \
+                           "Library Read Length, Flows, Number of Barcodes, Sample Tube Label, Chip Barcode, Chip Lot, " \
+                           "Chip Exp., Library Con., Total Bases, Key Signal, ISP Loading, Enrichment, Library ISPs (%)," \
+                           " Clonal, Final Library, Test Fragments, Adapter Dimer, Low Quality, Usable Reads, Total Reads," \
+                           " Mean Read Len., Media Read Len., Mode Read Len., Total Aligned Bases, Aligned Bases (%), " \
+                           "Unaligned Bases (%), Average Coverage Depth of Reference, TotalAlignedReads, AlignedReads, " \
+                           "UnalignedReads, AlignedReads (%), UnalignedReads (%), Mean Raw Accuracy 1× (%), AQ17 Bases," \
+                           " AQ20 Bases, Prefect Bases, AQ17_MeanLength (bp), AQ17_LongestAlignment (bp), " \
+                           "AQ17_MeanCoverageDepth, AQ20_MeanLength (bp), AQ20_LongestAlignment (bp), " \
+                           "AQ20_MeanCoverageDepth, Perfect_MeanLength (bp), Perfect_LongestAlignment (bp), " \
+                           "Perfect_MeanCoverageDepth, AddressableWells, WithISPs, Live, TestFragment, LibraryISPs, " \
+                           "FilteredPolyclonal, FilteredLowQuality, FilteredAdapterDimer, FinalLibraryISPs, " \
+                           "FilteredPolyclonal_r, Sample, OperationMode, FlowOrder, LibraryKey, TFKey, ChipCheck, " \
+                           "ChipData, ChipWafer, BarcodeSet, AnalysisFlows, runID"
+    sample_target_fields = "Report Name, Barcode Name, 建库编号, 测序编号, Library Type, Library kit, Reference, " \
+                           "Target regions, Bases, >=Q20 Bases (%), Reads, Mean Read Length (bp), Mapped Reads, " \
+                           "Mapped Reads (%), On Target (%), Mean Depth, Mean Depth / Mapped Reads (%), Uniformity, " \
+                           "Median Read Cov, Median Mol Cov, Media Mol Cov / Media Read Cov (%), " \
+                           "Median Read Cov / Reads (%), Median Mol Cov / Reads (%), LOD %, Variants, Hotspot Variants," \
+                           " Calculate Amplicons, Number of amplicons, Percent assigned amplicon reads," \
+                           " Average reads per amplicon, Uniformity of amplicon coverage," \
+                           " Amplicons with at least 1 read, Amplicons with at least 20 reads," \
+                           " Amplicons with at least 100 reads, Amplicons with at least 500 reads," \
+                           " Amplicons with no strand bias, Amplicons reading end-to-end," \
+                           " Amplicon base composition bias, Bases in target regions, Percent base reads on target," \
+                           " Average base coverage depth, Uniformity of base coverage, Target base coverage at 1x," \
+                           " Target base coverage at 20x, Target base coverage at 100x, Target base coverage at 500x," \
+                           " Target bases with no strand bias, Percent end-to-end reads"
+    overall_target_fields = [x.strip() for x in overall_target_fields.split(',')]
+    sample_target_fields = [x.strip() for x in sample_target_fields.split(',')]
+
+    # def match_fields(my_fields, ref_fields:list):
+    #     from fuzzywuzzy import fuzz
+    #     from fuzzywuzzy import process
+    #     choices = set(my_fields)
+    #     match_dict = OrderedDict()
+    #     for each in ref_fields:
+    #         match, score = process.extractOne(each, choices)
+    #         if score > 80:
+    #             match_dict[each] = match
+    #         else:
+    #             match_dict[each] = 'unknown'
+    #     return match_dict
+
+    # pprint(match_fields(final_overall.keys(), overall_target_fields))
+    # one_sample = list(sample_detail_dict.keys())[0]
+    # pprint(match_fields(sample_detail_dict[one_sample].keys(), sample_target_fields))
+
+    final_overall_match_dict = OrderedDict([
+        ('Run Name', final_overall['Run Name']),
+        ('Run Date', final_overall['Run Date']),
+        ('Report Name', final_overall['Report Name']),
+        ('Report Date', final_overall['Report Date']),
+        ('Research Application', 'unknown'),
+        ('Projects', final_overall['Projects']),
+        ('Chip Type', final_overall['Chip Type']),
+        ('Library Read Length', 'unknown'),
+        ('Flows', 'unknown'),
+        ('Number of Barcodes', 'unknown'),
+        ('Sample Tube Label', final_overall['Sample Tube Label']),
+        ('Chip Barcode', final_overall['Chip Barcode']),
+        ('Chip Lot', final_overall['Chip Lot Number']),
+        ('Chip Exp.', 'unknown'),
+        ('Library Con.', 'unknown'),
+        ('Total Bases', final_overall['Totabl Bases']),
+        ('Key Signal', final_overall['Key Signal']),
+        ('ISP Loading', final_overall['ISP Loading']),
+        ('Enrichment', 'unknown'),
+        ('Library ISPs (%)', 'unknown'),
+        ('Clonal', 1- int(final_overall['Filtered: Polyclonal'])/int(final_overall['Library ISPs'])),
+        ('Final Library', final_overall["Final Library ISPs"]),
+        ('Test Fragments', final_overall['Test Fragment']),
+        ('Adapter Dimer', final_overall['Filtered: Adapter Dimer']),
+        ('Low Quality', final_overall['Filtered: Low Quality']),
+        ('Usable Reads', final_overall['Usable Reads']),
+        ('Total Reads', final_overall['Total Reads']),
+        ('Mean Read Len.', final_overall['Mean']),
+        ('Media Read Len.', final_overall['Median']),
+        ('Mode Read Len.', final_overall['Mode']),
+        ('Total Aligned Bases', final_overall['Total Aligned Bases']),
+        ('Aligned Bases (%)', 'unknown'),
+        ('Unaligned Bases (%)', 'unknown'),
+        ('Average Coverage Depth of Reference', 'unknown'),
+        ('TotalAlignedReads', 'unknown'),
+        ('AlignedReads', final_overall['Aligned Reads']),
+        ('UnalignedReads', final_overall['Unaligned Reads']),
+        ('AlignedReads (%)', int(final_overall['Aligned Reads'])/int(final_overall['Total Reads'])),
+        ('UnalignedReads (%)', 1-int(final_overall['Aligned Reads'])/int(final_overall['Total Reads'])),
+        ('Mean Raw Accuracy 1× (%)', final_overall['Mean Raw Accuracy 1x']),
+        ('AQ17 Bases', final_overall['AQ17 Total Bases']),
+        ('AQ20 Bases', final_overall["AQ20_TotalNumberofBases[bp]"]),
+        ('Prefect Bases', final_overall["Perfect_TotalNumberofBases[bp]"]),
+        ('AQ17_MeanLength (bp)', final_overall['AQ17_MeanLength[bp]']),
+        ('AQ17_LongestAlignment (bp)', final_overall['AQ17_LongestAlignment[bp]']),
+        ('AQ17_MeanCoverageDepth', final_overall['AQ17_MeanCoverageDepth[x]']),
+        ('AQ20_MeanLength (bp)', final_overall['AQ20_MeanLength[bp]']),
+        ('AQ20_LongestAlignment (bp)', final_overall['AQ20_LongestAlignment[bp]']),
+        ('AQ20_MeanCoverageDepth', final_overall['AQ20_MeanCoverageDepth[x]']),
+        ('Perfect_MeanLength (bp)', final_overall['Perfect_MeanLength[bp]']),
+        ('Perfect_LongestAlignment (bp)', final_overall['Perfect_LongestAlignment[bp]']),
+        ('Perfect_MeanCoverageDepth', final_overall['Perfect_MeanCoverageDepth[x]']),
+        ('AddressableWells', final_overall['Addressable Wells']),
+        ('WithISPs', final_overall['With ISPs']),
+        ('Live', final_overall['Live']),
+        ('TestFragment', final_overall['Test Fragment']),
+        ('LibraryISPs', final_overall['Library ISPs']),
+        ('FilteredPolyclonal', final_overall['Filtered: Polyclonal']),
+        ('FilteredLowQuality', final_overall["Filtered: Low Quality"]),
+        ('FilteredAdapterDimer', final_overall["Filtered: Adapter Dimer"]),
+        ('FinalLibraryISPs', final_overall["Final Library ISPs"]),
+        ('FilteredPolyclonal_r', 'unknown'),
+        ('Sample', 'unknown'),
+        ('OperationMode', final_overall['Sequencer Operation Mode']),
+        ('FlowOrder', final_overall['Flow Order']),
+        ('LibraryKey', final_overall['Library Key']),
+        ('TFKey', final_overall['TF Key']),
+        ('ChipCheck', final_overall['Chip Check']),
+        ('ChipData', final_overall['Chip Data']),
+        ('ChipWafer', final_overall['Chip Wafer']),
+        ('BarcodeSet', final_overall['Barcode Set']),
+        ('AnalysisFlows', final_overall['Analysis Flows']),
+        ('runID', final_overall['runID'])
+    ])
+
+    pd.DataFrame(final_overall_match_dict, index=['all']).to_csv('overall.info.xls', index=False, header=True, sep='\t')
+
+    final_sample_detail = list()
+    for x, y in sample_detail_dict.items():
+        y = OrderedDict([
+            ('Report Name', final_overall['Report Name']),
+            ('Barcode Name', y['barcode_name']),
+            (u'建库编号', 'unknown'),
+            (u'测序编号', 'unknown'),
+            ('Library Type', 'unknown'),
+            ('Library kit', 'unknown'),
+            ('Reference', y['reference']),
+            ('Target regions', y['Target Regions']),
+            ('Bases', y["total_bases"]),
+            ('>=Q20 Bases (%)', '{}%'.format(round(int(y['Q20_bases'])/int(y["total_bases"])*100, 2))),
+            ('Reads', 'unknown'),
+            ('Mean Read Length (bp)', y["mean_read_length"]),
+            ('Mapped Reads', y['mapped_reads']),
+            ('Mapped Reads (%)', 'unknown'),
+            ('On Target (%)', y['Percent base reads on target']),
+            ('Mean Depth', y['mean_depth']),
+            ('Mean Depth / Mapped Reads (%)', 'unknown'),
+            ('Uniformity', y['uniformity']),
+            ('Median Read Cov', y['Median Read Cov']),
+            ('Median Mol Cov', y['Median Mol Cov']),
+            ('Media Mol Cov / Media Read Cov (%)', 'unknown'),
+            ('Median Read Cov / Reads (%)', 'unknown'),
+            ('Median Mol Cov / Reads (%)', 'unknown'),
+            ('LOD %', y['LOD %']),
+            ('Variants', y['variants']),
+            ('Hotspot Variants', y['hotspot_variants']),
+            ('Calculate Amplicons', 'unknown'),
+            ('Number of amplicons', y['Number of amplicons']),
+            ('Percent assigned amplicon reads', y['Percent assigned amplicon reads']),
+            ('Average reads per amplicon', y['Average reads per amplicon']),
+            ('Uniformity of amplicon coverage', y['Uniformity of amplicon coverage']),
+            ('Amplicons with at least 1 read', y['Amplicons with at least 1 read']),
+            ('Amplicons with at least 20 reads', y['Amplicons with at least 20 reads']),
+            ('Amplicons with at least 100 reads', y['Amplicons with at least 100 reads']),
+            ('Amplicons with at least 500 reads', y['Amplicons with at least 500 reads']),
+            ('Amplicons with no strand bias', y['Amplicons with no strand bias']),
+            ('Amplicons reading end-to-end', y['Amplicons reading end-to-end']),
+            ('Amplicon base composition bias', y['Amplicon base composition bias']),
+            ('Bases in target regions', y['Bases in target regions']),
+            ('Percent base reads on target', y['Percent base reads on target']),
+            ('Average base coverage depth', y['Average base coverage depth']),
+            ('Uniformity of base coverage', y['Uniformity of base coverage']),
+            ('Target base coverage at 1x', y['Target base coverage at 1x']),
+            ('Target base coverage at 20x', y['Target base coverage at 20x']),
+            ('Target base coverage at 100x', y['Target base coverage at 100x']),
+            ('Target base coverage at 500x', y['Target base coverage at 500x']),
+            ('Target bases with no strand bias', y['Target bases with no strand bias']),
+            ('Percent end-to-end reads', y['Percent end-to-end reads'])
+        ])
+        final_sample_detail.append(y)
+    pd.DataFrame(final_sample_detail).to_csv('sample_detail.xls', header=True, index=False, sep='\t')
 
 
 if __name__ == '__main__':
