b12a936188594fa3e600bd23132340c9ba2ac0c6
gudeqing <822466659@qq.com>
2019-06-26 11:20:25 +0800

    underscore replace

--- smallScriptsAtDunwill/findFastqAndLink.py
+++ smallScriptsAtDunwill/findFastqAndLink.py
@@ -2,7 +2,9 @@ import os
 import re
 
 
-def get_all_fastq_abs_path(path_lst: tuple, exp: str = '.*-(.*?)_combined_R[12].fastq.gz', r1_end_with='R1.fastq.gz'):
+def get_all_fastq_abs_path(path_lst: tuple, exp: str = '.*-(.*?)_combined_R[12].fastq.gz',
+                           r1_end_with='R1.fastq.gz', link_rawdata=False,
+                           replace_with_underscore=True):
     # ./180824_13_180905/Sample_R18054231-180824R-Pool-02-T180701R1L2/R18054231-180824R-Pool-02-T180701R1L2_combined_R2.fastq.gz
     result_dict = dict()
     for path in path_lst:
@@ -18,18 +20,22 @@ def get_all_fastq_abs_path(path_lst: tuple, exp: str = '.*-(.*?)_combined_R[12].
                         result_dict[sample][1].append(os.path.join(root, each))
 
     with open('fastq.info', 'w') as f:
-        os.mkdir('rawdata')
-        os.chdir('rawdata')
+        if link_rawdata:
+            os.mkdir('rawdata')
+            os.chdir('rawdata')
         for sample, lst in result_dict.items():
             read1 = sorted(lst[0])
             read2 = sorted(lst[1])
+            if replace_with_underscore:
+                sample = sample.replace('-', '_')
             f.write('{}\t{}\t{}\n'.format(sample, ';'.join(read1), ';'.join(read2)))
-            # make link
-            os.mkdir(sample)
-            for each in read1:
-                os.symlink(each, os.path.join(sample, os.path.basename(each)))
-            for each in read2:
-                os.symlink(each, os.path.join(sample, os.path.basename(each)))
+            if link_rawdata:
+                # make link
+                os.mkdir(sample)
+                for each in read1:
+                    os.symlink(each, os.path.join(sample, os.path.basename(each)))
+                for each in read2:
+                    os.symlink(each, os.path.join(sample, os.path.basename(each)))
 
 
 def link_fastq(fastq):
