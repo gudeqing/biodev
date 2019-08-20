7c6ad51b12eb4fef6e426a634ef9212e3bf7302f
gudeqing <822466659@qq.com>
2019-08-20 13:39:37 +0800

    增加画图

--- smallScriptsAtDunwill/metagene_diff.py
+++ smallScriptsAtDunwill/metagene_diff.py
@@ -3,6 +3,13 @@ import pandas as pd
 import scipy.stats as stats
 from functools import partial
 import statistics
+import matplotlib
+matplotlib.use('agg')
+import seaborn as sns
+import matplotlib.pyplot as plt
+sns.set(style="white")
+import itertools
+from scipy.stats import gmean
 
 
 def read_metagene_group(group_txt, format='gene2group'):
@@ -81,8 +88,15 @@ def diff_test(score_df, group_dict, cmp_list, method='mannwhitneyu', equal_var=T
             continue
         ctrl_samples = group_dict[ctrl]
         test_samples = group_dict[test]
+        # print(ctrl_samples)
+        # print(test_samples)
         ctrl_num = len(ctrl_samples)
         target_data = score_df[ctrl_samples+test_samples]
+        centered = target_data.sub(target_data.mean(axis=1), axis=0)
+        mean_centered = pd.DataFrame()
+        mean_centered[test] = centered[test_samples].mean(axis=1)
+        mean_centered[ctrl] = centered[ctrl_samples].mean(axis=1)
+
         if method == 'ranksums':
             test_func = stats.ranksums
         elif method == 'mannwhitneyu':
@@ -94,6 +108,8 @@ def diff_test(score_df, group_dict, cmp_list, method='mannwhitneyu', equal_var=T
                 test_func = stats.ttest_ind
             else:
                 test_func = partial(stats.ttest_ind, equal_var=False)
+        else:
+            raise Exception(f'{method} is not supported!')
         test_df = pd.DataFrame()
         test_df['pvalue'] = target_data.apply(lambda x:test_func(x[:ctrl_num], x[ctrl_num:])[1], axis=1)
         ctrl_median_exp = target_data[ctrl_samples].apply(statistics.median, axis=1)
@@ -106,17 +122,124 @@ def diff_test(score_df, group_dict, cmp_list, method='mannwhitneyu', equal_var=T
         test_df = test_df.join(target_data)
         test_df.sort_values(by='pvalue', inplace=True)
         test_df.to_csv(f'{prefix}{ctrl}_vs_{test}.metagene.{method}.xls', sep='\t')
+        # plot
+        mean_centered = mean_centered.loc[test_df.index]
+        mean_centered.index = [x[0] for x in mean_centered.index]
+        plot_exp_lines(mean_centered, out=f'{prefix}{ctrl}_vs_{test}.metagene.mean_centered.png')
 
 
 def metagene_diff(exp_matrix, sample_group, metagene_group, compare, prefix='',
                   metagene_group_format='gene2group', score='geometric_mean',
-                  method='mannwhitneyu', equal_var=True):
+                  method='mannwhitneyu', equal_var=True, box_x='gene',
+                  box_xlabel='MetaGene'):
+    """
+    给定表达矩阵, 样本分组信息和基因分组信息(metagene) 以及比较信息, 对meta基因做差异检验
+    :param exp_matrix: 表达矩阵文件路径
+    :param sample_group: 样本分组信息路径, 第一行为header
+    :param metagene_group: 基因分组信息,也即metagene信息文件路径. 文件无需header, 内容有两种格式可选:
+       (1) 第一列为metagene名,第二列为metagene的members，用逗号隔开 (2)第一列为基因名，第二列为metagene名
+    :param compare: 分组比较信息，第一列为对照组，第二列为实验组/测试组
+    :param prefix: 输出文件的前缀
+    :param metagene_group_format: gene2group表示第二种格式, group2gene表示第三种格式
+    :param score: 计算metagene score的方式, 默认geometric_mean, 即计算members的几何平均值，否则为算术平均值
+    :param method: 检验方法, 默认'mannwhitneyu', 还有[ranksums, wilcoxon(配对比较时可选用), ttest_ind]可选
+    :param equal_var: method为ttest_ind时可以指定的参数,默认做假设等方差检验
+    :param box_x: 画box图的参数 横轴以基因为单位(默认), 另可指定为sample, 此时横轴以sample为单位
+    :param box_xlabel: 画box图的参数 横轴的label, 默认为MetaGene
+    :return:
+    """
     metagene_group_dict = read_metagene_group(metagene_group, format=metagene_group_format)
     metagene_score_df = score_metagene(exp_matrix, metagene_group_dict, score=score)
+    box_data = metagene_score_df.copy()
+    box_data.index = [x for x, _ in box_data.index]
+    expr_box_plot(box_data, sample_group, x_col=box_x, xlabel=box_xlabel, prefix=prefix)
     sample_group_dict = read_sample_group(sample_group)
     compare_list = read_compare_info(compare, sample_group_dict)
     diff_test(metagene_score_df, sample_group_dict, compare_list,
               method=method, equal_var=equal_var, prefix=prefix)
+
+
+def _plot_multiline(data, out='multiline.png', annotate_at_end=False):
+    colors = ['b', 'r', 'g',  'y', 'c', 'pink', 'm', 'grey', 'darkgrey']
+    line_style = ['-', '-.', ':']
+    cmbs = list(itertools.product(colors, line_style))
+    plt.figure(figsize=(7, 8))
+    for ind, index in enumerate(data.index):
+        plt.plot(
+            range(data.shape[1]),
+            data.loc[index],
+            label=index,
+            color=cmbs[ind][0],
+            linestyle=cmbs[ind][1],
+            marker='o',
+            markeredgecolor=cmbs[ind][0],
+            markerfacecolor='w',
+            markersize=5
+        )
+        if annotate_at_end:
+            plt.annotate(
+                index,
+                xy=(data.shape[1]-1, data.loc[index][-1]),
+                fontsize=5
+            )
+    plt.xticks(range(data.shape[1]), data.columns)
+    plt.xlim(-0.2, data.shape[1])
+    if not annotate_at_end:
+        plt.legend(loc=1, fontsize='xx-small')
+    plt.savefig(out, dpi=300, bbox_inches='tight')
+
+
+def plot_exp_lines(data, index_col:list=0, sample_group=None, annotate_at_end=False,
+                   group_order:list=None, out='multiline.png'):
+    if sample_group is not None:
+        group_df = pd.read_csv(sample_group, index_col=0, header=0, sep=None, engine='python')
+        group_names = set(group_df.iloc[:, 0])
+        if group_order is None:
+            group_names = sorted(list(group_names))
+        else:
+            group_names = group_order
+        if type(data) == str:
+            data = pd.read_csv(data, index_col=index_col, header=0, sep=None, engine='python')
+        centered = data.sub(data.mean(axis=1), axis=0)
+        mean_centered = pd.DataFrame()
+        for name in group_names:
+            target_samples = list(group_df.loc[group_df.iloc[:, 0]==name].index)
+            mean_centered[name] = centered[target_samples].mean(axis=1)
+        # print(geometric.mean(axis=1))
+        plot_data = mean_centered
+    else:
+        if type(data) == str:
+            plot_data = pd.read_csv(data, index_col=index_col, header=0, sep=None, engine='python')
+        else:
+            plot_data = data
+
+    _plot_multiline(plot_data, out=out, annotate_at_end=annotate_at_end)
+
+
+def expr_box_plot(expr_matrix, sample_group, x_col='gene', xlabel=None, prefix=''):
+    if type(expr_matrix) == str:
+        data = pd.read_csv(expr_matrix, header=0, index_col=0, sep=None, engine='python')
+    else:
+        data = expr_matrix
+    data.index.name = 'Gene'
+    data = data.reset_index('Gene')
+    data = data.melt(id_vars=['Gene'], var_name='Sample', value_name='Expression')
+    group = pd.read_csv(sample_group, header=0, index_col=0, sep=None, engine='python')
+    group.index.name = 'Sample'
+    group_names = group.columns
+    group.reset_index('Sample', inplace=True)
+    data = data.merge(group, on='Sample')
+    # print(data.head())
+    for name in group_names:
+        if x_col.lower() == 'gene':
+            ax = sns.boxplot(x='Gene', y='Expression', hue=name, data=data)
+        else:
+            ax = sns.boxplot(x='Sample', y='Expression', hue=name, data=data)
+        ax.set_xticklabels(ax.get_xmajorticklabels(), fontsize=6, rotation=90)
+        if xlabel:
+            ax.set(xlabel=xlabel)
+        plt.savefig(f'{prefix}{name}.boxplot.png', dpi=300, bbox_inches='tight')
+        plt.close()
 
 
 if __name__ == '__main__':
