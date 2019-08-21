75b4128f82a3552fe9ae2e0944f684a78aed94f3
gudeqing <822466659@qq.com>
2019-08-21 13:17:10 +0800

    使用statsmodels做多重校正，我的之前得校正方法存在瑕疵：经过调查发现，padjust的校正还有一步我忽略了：如果一个pvalue比另一个小，但经过校正后，其又比另一个大，此时需要用较小的fdr替代哪个原本较小的pvalue的fdr，最后，这两个的pvalue的fdr一样了。

--- kegg_enrichment2.py
+++ kegg_enrichment2.py
@@ -1,6 +1,7 @@
 # coding=utf-8
 from collections import defaultdict
 from scipy.stats import hypergeom
+from statsmodels.stats.multitest import multipletests
 import numpy as np
 import os
 import argparse
@@ -294,104 +295,6 @@ def prepare_hypergeom_data(class_gene_dict, gene_class_dict, deg_dict, total_gen
         yield study_hitnumber, pop_number, pop_hitnumber, study_number, each_class, associated_diff_info, link, path_name, typeII, typeI
 
 
-def multtest_correct(p_values, methods=3):
-    """
-    1. Bonferroni
-    2. Bonferroni Step-down(Holm)
-    3. Benjamini and Hochberg False Discovery Rate
-    4. FDR Benjamini-Yekutieli
-    :param pvalue_list:
-    :param methods:
-    :return: np.array
-    """
-
-    def fdrcorrection(pvals, alpha=0.05, method='indep', is_sorted=False):
-        '''pvalue correction for false discovery rate
-        This covers Benjamini/Hochberg for independent or positively correlated and
-        Benjamini/Yekutieli for general or negatively correlated tests. Both are
-        available in the function multipletests, as method=`fdr_bh`, resp. `fdr_by`.
-        Parameters
-        ----------
-        pvals : array_like
-            set of p-values of the individual tests.
-        alpha : float
-            error rate
-        method : {'indep', 'negcorr')
-        Returns
-        -------
-        rejected : array, bool
-            True if a hypothesis is rejected, False if not
-        pvalue-corrected : array
-            pvalues adjusted for multiple hypothesis testing to limit FDR
-        Notes
-        -----
-        If there is prior information on the fraction of true hypothesis, then alpha
-        should be set to alpha * m/m_0 where m is the number of tests,
-        given by the p-values, and m_0 is an estimate of the true hypothesis.
-        (see Benjamini, Krieger and Yekuteli)
-        The two-step method of Benjamini, Krieger and Yekutiel that estimates the number
-        of false hypotheses will be available (soon).
-        Method names can be abbreviated to first letter, 'i' or 'p' for fdr_bh and 'n' for
-        fdr_by.
-        '''
-
-        def _ecdf(x):
-            '''
-            no frills empirical cdf used in fdrcorrection
-            '''
-            nobs = len(x)
-            return np.arange(1, nobs + 1) / float(nobs)
-
-        pvals = np.asarray(pvals)
-        if not is_sorted:
-            pvals_sortind = np.argsort(pvals)
-            pvals_sorted = np.take(pvals, pvals_sortind)
-        else:
-            pvals_sorted = pvals  # alias
-
-        if method in ['i', 'indep', 'p', 'poscorr']:
-            ecdffactor = _ecdf(pvals_sorted)
-        elif method in ['n', 'negcorr']:
-            cm = np.sum(1. / np.arange(1, len(pvals_sorted) + 1))  # corrected this
-            ecdffactor = _ecdf(pvals_sorted) / cm
-        else:
-            raise ValueError('only indep and negcorr implemented')
-        reject = pvals_sorted <= ecdffactor * alpha
-        if reject.any():
-            rejectmax = max(np.nonzero(reject)[0])
-            reject[:rejectmax] = True
-
-        pvals_corrected_raw = pvals_sorted / ecdffactor
-        pvals_corrected = np.minimum.accumulate(pvals_corrected_raw[::-1])[::-1]
-        del pvals_corrected_raw
-        pvals_corrected[pvals_corrected > 1] = 1
-        if not is_sorted:
-            pvals_corrected_ = np.empty_like(pvals_corrected)
-            pvals_corrected_[pvals_sortind] = pvals_corrected
-            del pvals_corrected
-            reject_ = np.empty_like(reject)
-            reject_[pvals_sortind] = reject
-            return reject_, pvals_corrected_
-        else:
-            return reject, pvals_corrected
-
-    pvalue_list = list(p_values)
-    n = len(pvalue_list)
-    if methods == 1:
-        fdr = [eachP * n for eachP in pvalue_list]
-    elif methods == 2:
-        sorted_pvalues = sorted(pvalue_list)
-        fdr = [eachP * (n - sorted_pvalues.index(eachP)) for eachP in pvalue_list]
-    elif methods == 3:
-        sorted_pvalues = sorted(pvalue_list)
-        fdr = [eachP * n / (sorted_pvalues.index(eachP) + 1) for eachP in pvalue_list]
-    elif methods == 4:
-        _, fdr = fdrcorrection(pvalue_list, alpha=0.05, method='negcorr', is_sorted=False)
-    fdr = np.array(fdr)
-    fdr[fdr > 1] = 1.
-    return fdr
-
-
 def hypergeom_test(data, sort_fdr=True):
     p_value_list = []
     ratio_in_study_list = []
@@ -414,7 +317,7 @@ def hypergeom_test(data, sort_fdr=True):
         typeIIs.append(typeII)
         typeIs.append(typeI)
 
-    q_value_list = multtest_correct(p_value_list)
+    q_value_list = multipletests(p_value_list, method='fdr_bh')[1]
     number = len(q_value_list)
     # print(number)
     databases = ['KEGG PATHWAY'] * number
