4b49f8c7d3e1f9a55427d1ab5bd1ac98f124e30c
gudeqing <822466659@qq.com>
2019-09-09 17:23:31 +0800

    拆分fastq脚本开发

---
+++ smallScriptsAtDunwill/demultiplex_fastq.py
@@ -0,0 +1,81 @@
+import os
+import gzip
+import pandas as pd
+
+
+def read_sample_info(in_file):
+    table = pd.read_csv(in_file, sep=None, header=0, index_col=0, engine='python')
+    table.columns = ['p7index', 'p5index']
+    table.index.name = 'sample'
+    pair_index_dict = dict()
+    for sample in table.index:
+        p7index = table.loc[sample, 'p7index']
+        p5index = table.loc[sample, 'p5index']
+        indexes = tuple(sorted([p7index, p5index]))
+        if indexes in pair_index_dict:
+            raise Exception(f'indexes of {sample} is same as '
+                            f'{pair_index_dict[indexes]}')
+        pair_index_dict[indexes] = sample
+    return pair_index_dict
+
+
+def fastq_parser(fastq):
+    # not used currently
+    with gzip.open(fastq, 'rb') as f:
+        seq_info = list()
+        seq_info.append(f.readline())
+        seq_info.append(f.readline())
+        seq_info.append(f.readline())
+        seq_info.append(f.readline())
+        for line in f:
+            if line.startswith(b'@'):
+                yield seq_info
+                seq_info = [line]
+            else:
+                seq_info.append(line)
+        else:
+            yield seq_info
+
+
+def split_fastq(fastq, sample_info):
+    """
+    拆分fastq
+    :param fastq: undetermined fastq file
+    :param sample_info: 第一行为header，第一列为样本名，第二列为p7index，第三列为p5index
+    :return:
+    """
+    # @<instrument>:<run number>:<flowcell ID>:<lane>:<tile>:<x-pos>:<y-pos>
+    # <read>:<is filtered>:<control number>:<index sequence>
+    pair_index_dict = read_sample_info(sample_info)
+    # print(pair_index_dict)
+    write_obj_dict = dict()
+    with gzip.open(fastq, 'rb') as f:
+        while True:
+            line = f.readline()
+            if not line:
+                break
+            if line.startswith(b'@'):
+                header, detail = line.strip().decode().split()
+                lane = '{:0>3d}'.format(int(header.split(':')[3]))
+                read_num, filtered, ctrl_num, indexes = detail.split(':')
+                indexes = tuple(sorted(indexes.split('+')))
+                sample = pair_index_dict.get(indexes)
+                if sample:
+                    w_id = sample+f'_R{read_num}'
+                    if w_id not in write_obj_dict:
+                        write_obj = gzip.open(f'{sample}_S1_L{lane}_R{read_num}_001.fastq.gz', 'w')
+                        write_obj_dict[w_id] = write_obj
+                    else:
+                        write_obj = write_obj_dict[w_id]
+                    write_obj.write(line)
+                    write_obj.write(f.readline())
+                    write_obj.write(f.readline())
+                    write_obj.write(f.readline())
+    # close writing obj
+    for _, obj in write_obj_dict.items():
+        obj.close()
+
+
+if __name__ == '__main__':
+    from xcmds import xcmds
+    xcmds.xcmds(locals(), include=['split_fastq'])
