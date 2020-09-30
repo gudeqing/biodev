2f9fc0e00835d916b811bf347abe94c59964190e
gudeqing <822466659@qq.com>
2020-09-30 17:30:32 +0800

    none

--- plots/tmp.p.py
+++ plots/tmp.p.py
@@ -6,6 +6,7 @@ matplotlib.use('agg')
 from matplotlib import pyplot as plt
 import seaborn as sns
 import numpy as np
+from scipy.stats import gaussian_kde
 
 d2 = pd.DataFrame()
 
@@ -23,4 +24,31 @@ ax.tick_params('x', labelsize=7)
 plt.savefig(f'boxplot.pdf', bbox_inches='tight')
 plt.close()
 
+# ax = axes[i].twinx()
+# density = gaussian_kde(s1)
+# xs = range(s1.min()-2, s1.max()+2)
+# ax.plot(xs, density(xs), label='Normal', alpha=0.7, color='darkgreen')
+# density = gaussian_kde(s2)
+# xs = range(s2.min() - 2, s2.max() + 2, )
+# ax.plot(xs, density(xs), label='Tumor', alpha=0.7, color='darkorange')
+fig, ax = plt.subplots()
+data = np.random.normal(size=100)
+ax.hist(data)
+from scipy.stats import gaussian_kde
+s1 = pd.Series(data)
+density = gaussian_kde(s1)
+xs = np.linspace(s1.min()-2, s1.max()+2, 100)
+ax2 = ax.twinx()
+ax2.plot(xs, density(xs), label='Normal', alpha=0.7, color='darkgreen')
+plt.savefig(f'hist_density.pdf', bbox_inches='tight')
 
+
+def hist_density(data, ax=None):
+    if ax is None:
+        fig, ax = plt.subplots()
+    ax.hist(data)
+    s1 = pd.Series(data)
+    density = gaussian_kde(s1)
+    xs = np.linspace(s1.min() - 2, s1.max() + 2, 100)
+    ax2 = ax.twinx()
+    ax2.plot(xs, density(xs), label='Normal', alpha=0.7, color='darkgreen')
