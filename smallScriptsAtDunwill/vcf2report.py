02d025594836dcfdc06000adb7bbef9b56f2da9c
gudeqing <822466659@qq.com>
2020-10-27 11:37:39 +0800

    none

--- smallScriptsAtDunwill/vcf2report.py
+++ smallScriptsAtDunwill/vcf2report.py
@@ -118,17 +118,22 @@ def annovar_annotation(vcf):
     annovar = "/nfs2/software/annovar/ANNOVAR_2019.10.24/table_annovar.pl"
     annovardb = "/nfs2/database/annovar/humandb/"
     cmd = f"{annovar} {vcf} {annovardb} -buildver hg19 -out {vcf} -remove "
-    protocols =','.join([
-        'refGeneWithVer', 'cytoBand', 'avsnp150', 'snp138NonFlagged',
+    g_r_pro = ['refGeneWithVer', 'cytoBand']
+    # f_pro = [
+    #     'avsnp150', 'cosmic90', 'clinvar_20200316', 'CancerHotspots',
+    #     'esp6500siv2_all', '1000g2015aug_all', 'exac03', 'gnomad_exome'
+    # ]
+    f_pro = [
+        'avsnp150', 'snp138NonFlagged',
         'AVENIO', 'EpioneHS',
         'cosmic90', 'Oncomine', 'CGI', 'CBMDB', 'CIVIC', 'DOCM', 'CHASMplus',
         'IntOGen', 'TCGA_PCDM', 'TCGA', 'icgc21', 'CancerHotspots', 'nci60', 'clinvar_20200316',
         'esp6500siv2_all', '1000g2015aug_all', 'exac03', 'gnomad_exome', 'gme', 'cg69', 'hrcr1',
         'intervar_20180118', 'dbnsfp33a'
-    ])
-    operations = 'g,r,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f'
-    args = '-hgvs,-hgvs,-hgvs,-hgvs,-hgvs,-hgvs,-hgvs,-hgvs,-hgvs,-hgvs,-hgvs,-hgvs,-hgvs,' \
-           '-hgvs,-hgvs,-hgvs,-hgvs,-hgvs,-hgvs,-hgvs,-hgvs,-hgvs,-hgvs,-hgvs,-hgvs,-hgvs,-hgvs,-hgvs,-hgvs'
+    ]
+    protocols =','.join(g_r_pro+f_pro)
+    operations = 'g,r,' + ','.join(['f']*len(f_pro))
+    args = ','.join(['-hgvs']*len(g_r_pro+f_pro))
     cmd += f'-protocol {protocols} '
     cmd += f'-operation {operations} '
     cmd += f"--argument '{args}' "
