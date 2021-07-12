2108491e08f644b2d0a2326b41e502ec9facf008
gudeqing <822466659@qq.com>
2021-07-12 15:42:58 +0800

    参数添加wdl属性和‘fix'类型，帮助wdl正确生成，这个需要掌握一定的wdl知识

--- bbt/DNAseq/example_pipeline.py
+++ bbt/DNAseq/example_pipeline.py
@@ -2,7 +2,7 @@ from wf import Argument, Output, Command, Workflow
 
 """
 要想生成正确的wdl:
-1.一定要正确定义参数的类型，比如是否为”infile“ or "indri",
+1.一定要正确定义参数的类型，比如是否为”infile“ or "indir",
  type is one of ['str', 'int', 'float', 'bool', 'infile', 'indir', 'fix']
 """
 
@@ -13,11 +13,11 @@ def fastp():
     cmd.runtime.image = 'gudeqing/fastp:0.21.0'
     cmd.runtime.tool = 'fastp'
     # 可以直接用访问属性的方式添加参数，这个得益于使用Munch对象而不是原生字典
-    cmd.args.read1 = Argument(prefix='-i ', type='infile')
-    cmd.args.read2 = Argument(prefix='-I ', type='infile', level='required')
+    cmd.args.read1 = Argument(prefix='-i ', type='infile', desc='read1 fastq file')
+    cmd.args.read2 = Argument(prefix='-I ', type='infile', desc='read2 fastq file')
     # 当然，可以直接用字典的方式添加参数
-    cmd.args['out1'] = Argument(prefix='-o ', type='str', level='required')
-    cmd.args['out2'] = Argument(prefix='-O ', type='str', level='required')
+    cmd.args['out1'] = Argument(prefix='-o ', type='str', desc='clean read1 output fastq file')
+    cmd.args['out2'] = Argument(prefix='-O ', type='str', level='clean read2 output fastq file')
     cmd.outputs['out1'] = Output(path="~{out1}")
     cmd.outputs['out2'] = Output(path="~{out2}")
     return cmd
@@ -33,11 +33,11 @@ def salmon():
     cmd.runtime.cpu = 2
     cmd.runtime.tool = 'salmon quant'
     cmd.args.libType = Argument(prefix='--libType ', default='A')
-    cmd.args['indexDir'] = Argument(prefix='-i ', type='indir', level='required')
-    cmd.args['read1'] = Argument(prefix='-1 ', type='infile', level='required')
-    cmd.args['read2'] = Argument(prefix='-2 ', type='infile', level='required')
-    cmd.args['outDir'] = Argument(prefix='-o ', type='str', level='optional', default='quant')
-    cmd.args['gcBias'] = Argument(prefix='--gcBias ', type='bool', default=True)
+    cmd.args['indexDir'] = Argument(prefix='-i ', type='indir', desc='transcript fasta index directory')
+    cmd.args['read1'] = Argument(prefix='-1 ', type='infile', desc='read1 fastq file')
+    cmd.args['read2'] = Argument(prefix='-2 ', type='infile', desc='read2 fastq file')
+    cmd.args['outDir'] = Argument(prefix='-o ', type='str', level='optional', default='quant', desc='output directory')
+    cmd.args['gcBias'] = Argument(prefix='--gcBias ', type='bool', default=True, desc='perform gc Bias correction')
     cmd.outputs['transcript'] = Output(path="~{outDir}" + "/quant.sf", locate='quant')
     cmd.outputs['outDir'] = Output(path="~{outDir}", locate='quant', type='Directory')
     return cmd
@@ -50,7 +50,7 @@ def quant_merge():
     cmd.runtime.image = "combinelab/salmon:latest"
     cmd.runtime.tool = 'salmon quantmerge'
     # 下面的quants参数对应的是目录，所以type='indir'
-    cmd.args['quants'] = Argument(prefix="--quants ", array=True, type='indir')
+    cmd.args['quants'] = Argument(prefix="--quants ", array=True, type='indir', desc='salmon quant dir list')
     cmd.args['names'] = Argument(prefix='--names ', array=True, level='optional')
     cmd.args['column'] = Argument(prefix='--column ', default='TPM')
     cmd.args['genes'] = Argument(prefix='--genes ', type='bool', default=False)
@@ -84,7 +84,7 @@ def pipeline():
         task_id = task.task_id
         args['read1'].value = r1
         # 给参数添加wdl属性,目的是为了正确生成wdl流程，如果无需生成wdl流程，则无需添加wdl属性。
-        # 下面代码中的each表示wdl中对array进行循环时的临时变量
+        # 下面代码中的each表示wdl中对array进行循环时的临时变量, each结构：{sample:{r1:r2}}
         args['read1'].wdl = "each.right.left"
         args['read2'].value = r2
         args['read2'].wdl = "each.right.right"
@@ -118,26 +118,28 @@ def pipeline():
     task, args = wf.add_task(quant_merge())
     task.cmd.meta.name = 'MergeTranscriptTPM'
     task.depends = merge_depends
+    # 针对当前任务，column 和 genes 参数需要固定，不允许修改,用 'fix' 表示，便于wdl的正确生成。
     args['column'].value = 'TPM'
     args['column'].type = 'fix'
     args['genes'].value = False
     args['genes'].type = 'fix'
     args['quants'].value = [wf.tasks[task_id].outputs['outDir'].path for task_id in task.depends]
     args['quants'].wdl = f'{wf.tasks[merge_depends[0]].cmd.meta.name}.outDir'
-    task.outputs['result'] = Output(path=args['out'].value)
+    task.outputs['result'] = Output(path=f'merged.{args["column"].value}.txt', type='File')
 
     # merge transcript Count
     task, args = wf.add_task(quant_merge())
     task.cmd.meta.name = 'MergeTranscriptCount'
     task.depends = merge_depends
     # 针对当前任务，column 和 genes 参数需要固定，不允许修改
-    args['column'].value = 'ReadNumber'
+    args['column'].value = 'NumReads'
     args['column'].type = 'fix'
     args['genes'].value = False
     args['genes'].type = 'fix'
     args['quants'].value = [wf.tasks[task_id].outputs['outDir'].path for task_id in task.depends]
     args['quants'].wdl = f'{wf.tasks[merge_depends[0]].cmd.meta.name}.outDir'
-    task.outputs['result'] = Output(path=args['out'].value)
+    args['out'].default = f'merged.{args["column"].value}.txt'
+    task.outputs['result'] = Output(path=f'merged.{args["column"].value}.txt', type='File')
 
     for _, task in wf.tasks.items():
         # print(task.task_id)
