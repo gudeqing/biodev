513709cd9f753225c0abae4dd109eef6c6e75028
gudeqing <822466659@qq.com>
2021-08-04 12:13:05 +0800

    增加表头说明

--- bbt/wdlJson2xlsx.py
+++ bbt/wdlJson2xlsx.py
@@ -7,6 +7,19 @@ import pandas as pd
 print('usage: python wdlJson2xlsx.py <input.json> [wf.wdl] ')
 
 infile = sys.argv[1]
+attr_annot = {
+    "name": "参数名,将在页面上显示，如star.index_dir；不填则默认通过分割key获得",
+    "type": "表示参数类型，支持填写以下类型：str,int, float, bool, infile(表示为输入文件）, indir（表示为输入目录）；不填写则默认为str类型",
+    "level": "表示参数是否必需，用‘optional’ 或‘required’表示；不填则默认为optional",
+    "array": "表示参数是否接收多个值，用‘yes' 或 'no'表示；不填则默认为no",
+    "range": "表示参数的可选内容，如['A','B','C']；不填则表示参数无可选内容",
+    "default": "表示参数的默认值, 不填则表示该参数无默认值",
+    "format": "表示对输入文件的格式要求说明，例如'vcf,vcf.gz'；不填则默认对格式无要求",
+    "order": "表示参数的先后顺序, 数字越小越靠前；不填则默认为1",
+    "desc": "表示参数说明，不填则默认显示key填写的内容",
+}
+
+key_annot = "表示参数ID, 必填项，需要跟流程中定义的保持一致，如pipeline.star.index_dir, 我们有一个脚本可以解析womtool生成的参数json文件从而生成当前文档的初稿"
 
 with open(infile) as f:
     arg_dict = json.load(f)
@@ -58,14 +71,21 @@ with open(infile) as f:
         tmp['order'] = 1
         tmp['desc'] = key
 
-result = {x: result[x] for x in sorted(result.keys())}
+result[key_annot] = attr_annot
+result = {x: result[x] for x in [key_annot] + sorted(set(result.keys()) - {key_annot})}
 
-#  获取参数注释
+#  获取参数注释, 要求wdl文件中每一个task定义时都定义parameter_meta，而且parameter_meta中参数的描述中必需包含desc字段，如下：
+#  parameter_meta {
+#         sample: {desc: "sample name", type='xx', other='xxx'}
+#         genome: {desc: "genome fasta file"}
+
 if len(sys.argv) >= 3:
     wdl_file = sys.argv[2]
+    # 获取task 列表
     with open(wdl_file) as f:
         task_names = re.findall(r'task\s+(.*)\{', f.read())
 
+    # 获取task别名和task本名的映射信息
     with open(wdl_file) as f:
         match = re.findall(r'call\s+(.*)\s+as\s+(.*)\s+\{', f.read())
         print(match)
@@ -73,16 +93,23 @@ if len(sys.argv) >= 3:
             task_mapping = dict(match)
         else:
             task_mapping = dict()
+
+    # 将别名转化为本名
     task_names = [task_mapping[x] if x in task_mapping else x for x in task_names]
 
+    # 提取每个task对应的parameter_meta
     with open(wdl_file) as f:
         # 最后根据是"\s}\n"定位parameter的结束
         arg_desc = re.findall(r'parameter_meta\s+\{(.*?)\s+\}\n', f.read(), flags=re.S)
         arg_desc = [x.strip() for x in arg_desc]
 
+    # 将task和parameter_meta按照顺序对应起来, 因为时按序对应，因此要求每个task都有parameter_meta部分，否则容易错配
     desc_dict = dict(zip(task_names, arg_desc))
 
+    # 获取desc信息
     for k, v in result.items():
+        if k == key_annot:
+            continue
         name = v['name']
         if len(name.split('.', 1)) == 1:
             continue
@@ -96,6 +123,7 @@ if len(sys.argv) >= 3:
         else:
             print(f'no desc found for {name} !')
 
+# 生成最后的xlsx格式的参数说明文件，用于XDP
 data = pd.DataFrame(result).T
 data.index.name = 'key'
 data.to_excel('args.detail.xlsx')
